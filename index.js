9421d9baabbf11202cb47406c4de300dfed49825
parthkotwe73@gmail.com <your-email@example.com>
2026-05-04 18:07:32 +0530

    added schema for user,creator,course and purchesCourses

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const Port = 3000;
-
+const mongoose = require("mongoose");
+const {database} = require("./database/schema");
 
 app.listen(Port, ()=>{
     console.log("Server is running on port 3000"+ Port);
